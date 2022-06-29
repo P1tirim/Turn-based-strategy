149ef16c485f9a942f1a09f9eed93805e87ba1d2
P1tirim <andrey.orishev@mail.ru>
2022-06-29 19:44:56 +0300

    v0.13
    Now the cells that you can move to are marked

---
+++ FirstStrategy/Assets/Scripts/Cells.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cells : MonoBehaviour
+{
+    public int moveSpeed = 1;
+    public GameObject particle;
+
+    int currentPositionX = 0;
+    int currentPositionY = 2;
+    GameObject[,] cells = new GameObject[5, 5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CellsInArray();
+        SpawnParticle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Put cells in array for further use
+    void CellsInArray()
+    {
+        int child = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                cells[i, j] = this.gameObject.transform.GetChild(child).gameObject;
+                child++;
+            }
+        }
+    }
+
+    //Span particle which show the cells where you can go
+    void SpawnParticle()
+    {
+        
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (currentPositionX == i && currentPositionY == j) continue;
+                if(Mathf.Abs(currentPositionX-i) <= moveSpeed && Mathf.Abs(currentPositionY-j) <= moveSpeed)
+                {
+                    GameObject cell = cells[i, j];
+                    Vector3 vector;
+                    vector.x = cell.transform.position.x + 0.16f;
+                    vector.y = cell.transform.position.y;
+                    vector.z = cell.transform.position.z;
+
+                    Instantiate(particle, vector, Quaternion.Euler(-90, 0, 0));
+                }
+            }
+        }
+    }
+}
